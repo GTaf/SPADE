031079985c0b9098fdfcdda767e64a5a83fd1af8
GTaf <auredu94@gmail.com>
2018-05-25 23:04:31 +0100

    Path mistake

--- bin/addToSystemd.sh
+++ bin/addToSystemd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp SPADE.service /stc/systemd/system
+sudo cp SPADE.service /etc/systemd/system
 sudo chmod u+x spade
 systemctl daemon-reload
 sudo systemctl enable SPADE.service
