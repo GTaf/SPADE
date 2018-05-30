10a176d6bd7e34ef1ba3698f204eb3aeece86f9c
vagrant <vagrant@localhost.localdomain>
2018-05-30 13:13:02 -0700

    disabling SELinux in script

--- bin/addToSystemd.sh
+++ bin/addToSystemd.sh
@@ -2,5 +2,6 @@
 
 sudo cp SPADE.service /etc/systemd/system
 sudo chmod u+x spade
+sudo sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
 systemctl daemon-reload
 sudo systemctl enable SPADE.service
