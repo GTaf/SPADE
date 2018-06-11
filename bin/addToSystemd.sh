8a173d053c5abd77d90bd899398fe3c7be5738ad
GTaf <auredu94@gmail.com>
2018-06-11 16:35:40 -0700

    more general way to use systemd

--- bin/addToSystemd.sh
+++ bin/addToSystemd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+SPADE_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
 
-sudo cp SPADE.service /etc/systemd/system
+echo $SPADE_ROOT
+
+sudo sed -i 's/ExecStart=.*/ExecStart=$SPADE_ROOT/bin/spade start/g' $SPADE_ROOT/bin/SPADE.service
+
+sudo cp $SPADE_ROOT/bin/SPADE.service /etc/systemd/system
 sudo chmod u+x spade
 sudo sed -i 's/SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
 systemctl daemon-reload
